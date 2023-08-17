877f8076aed86fc5d5d61edc49fb8d55f4d79e12
includeFukari <tatiosey@protonmail.com>
2023-08-17 11:28:15 +0200

    day 1 solution

---
+++ day_1/main.rb
@@ -0,0 +1,29 @@
+unfiltered_calories_amount = []
+filtered_calories_amount = []
+index = 0
+greatest_amount_of_calories = 0
+
+File.open("input.txt", 'r') do |file|
+    file.each_line do |line|
+        (line == "\n") ? unfiltered_calories_amount << line : unfiltered_calories_amount << line.to_i
+    end
+end
+
+unfiltered_calories_amount.each do |element|
+    if (filtered_calories_amount[index].nil?)
+        filtered_calories_amount[index] = []
+    end
+    (element != "\n") ? filtered_calories_amount[index] << element : index += 1
+end
+
+filtered_calories_amount.map! { |element|
+    element.reduce { |a, e| a + e }
+}
+
+filtered_calories_amount.each do |element|
+    if (element > greatest_amount_of_calories)
+        greatest_amount_of_calories = element
+    end
+end
+
+puts greatest_amount_of_calories
