789c717a0dd6d4e2f67de1ebcbfc2e188fb41304
includeFukari <tatiosey@protonmail.com>
2023-08-20 16:31:19 +0200

    Day 2. Changed switch to hash and deleted map.

--- day_2/main.rb
+++ day_2/main.rb
@@ -1,23 +1,20 @@
 matches = []
+scores = {
+    "AX" => 4,
+    "AY" => 8,
+    "AZ" => 3,
+    "BX" => 1,
+    "BY" => 5,
+    "BZ" => 9,
+    "CX" => 7,
+    "CY" => 2,
+    "CZ" => 6
+}
 
 File.open("input.txt", 'r') do |file|
     file.each_line do |line|
-        matches << "#{line[0]}#{line[2]}"
+        matches << scores["#{line[0]}#{line[2]}"]
     end
 end
-
-matches.map! { |element|
-    case element
-        when "AX" then 4
-        when "AY" then 8
-        when "AZ" then 3
-        when "BX" then 1
-        when "BY" then 5
-        when "BZ" then 9
-        when "CX" then 7
-        when "CY" then 2
-        when "CZ" then 6
-    end
-}
 
 puts matches.sum
